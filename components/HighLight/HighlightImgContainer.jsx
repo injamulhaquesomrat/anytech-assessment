446a4c0e691aa684f01efb7e31057a0acf25831c
haquesomrat <ihsomrat.official@gmail.com>
2025-02-02 23:10:22 +0600

    Update HighlightImgContainer styling for improved layout and responsiveness

--- components/HighLight/HighlightImgContainer.jsx
+++ components/HighLight/HighlightImgContainer.jsx
@@ -14,34 +14,33 @@ const HighlightImgContainer = () => {
           height={1067}
         />
         <Image
-          className="absolute top-0 left-0 z-0 object-cover rotate-180"
+          className="absolute -top-8 md:-top-10 lg:-top-16 left-0 z-0 object-cover rotate-180"
           src="/backgrounds/WaveLinesMobile2.svg"
           alt="Wave"
           width={1023}
           height={100}
         />
         <Image
-          className="absolute top-8 right-4 z-10 object-cover"
+          className="absolute top-14 lg:top-20 right-0 z-10 object-cover aspect-square h-[80px] w-[80px] lg:h-[114px] lg:w-[114px]"
           src="/images/bank-icon.svg"
           alt="Wave"
           width={115}
           height={114}
         />
         <Image
-          className="absolute top-16 left-8 z-10 object-cover"
+          className="absolute top-16 lg:top-20 left-3 lg:left-[5%] z-10 object-cover h-[53px] w-[53px] lg:h-[73px] lg:w-[73px]"
           src="/images/card-icon.svg"
           alt="Wave"
           width={73}
           height={73}
         />
         <Image
-          className="absolute top-40 left-20 z-10 object-cover"
+          className="absolute top-40 lg:top-48 left-14 lg:left-[15%] z-10 object-cover h-[58px] w-[58px] lg:h-[87px] lg:w-[87px]"
           src="/images/analytics-icon.svg"
           alt="Wave"
           width={87}
           height={87}
         />
-        
       </div>
       <div className="md:hidden">
         <HighlightText />
